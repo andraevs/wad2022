9baaabfcc2a11bb2eda4a9f7e04debb46b7ddbff
Vas <andraevs@gmail.com>
2022-05-02 21:29:41 +0300

    x

--- spring-react-start/src/main/java/com/example/mvcproducts/security/SecurityConfig.java
+++ spring-react-start/src/main/java/com/example/mvcproducts/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**")
             .permitAll();
     http.csrf().disable();
+    http.cors();
 
 
 
